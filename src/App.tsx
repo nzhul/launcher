293c70021540b2b5dfcb5ad571d7eec06d7308a2
nzhul <dobromirivanov1@gmail.com>
2023-03-08 13:23:14 +0200

    install mui

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,30 @@
+import { Button, Container } from "@mui/material";
 import React from "react";
 import * as ReactDOM from "react-dom/client";
 import SimpleList from "./renderer/common/SimpleList";
+import NavBar from "./renderer/layout/Navbar";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <div>
-    <h1>Directory files</h1>
-    <SimpleList
+  <>
+    <NavBar />
+
+    <Container data-testid="page-container" sx={{ marginTop: 5 }}>
+      <Button variant="contained">
+        <span>Contained</span>
+      </Button>
+
+      <SimpleList
       items={[
         { id: 1, name: "file1" },
         { id: 2, name: "file2" },
       ]}
     />
-  </div>
+    </Container>
+  </>
 );
 
 // Clears the annoying "Download the React DevTools for a better development experience" text
---
+++ src/renderer/layout/Navbar.tsx
@@ -0,0 +1,7 @@
+import Box from "@mui/material/Box";
+
+const NavBar = () => {
+  return <Box>This is the main navbar</Box>;
+};
+
+export default NavBar;
