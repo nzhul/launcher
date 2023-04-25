1cb9918c4e39469a59cf097fddb73dc41732e999
nzhul <dobromirivanov1@gmail.com>
2023-04-25 22:14:17 +0300

    apply some launcher windows installer settings

--- forge.config.ts
+++ forge.config.ts
@@ -1,21 +1,28 @@
-import type { ForgeConfig } from "@electron-forge/shared-types";
-import { MakerSquirrel } from "@electron-forge/maker-squirrel";
-import { MakerZIP } from "@electron-forge/maker-zip";
-import { MakerDeb } from "@electron-forge/maker-deb";
-import { MakerRpm } from "@electron-forge/maker-rpm";
-import { WebpackPlugin } from "@electron-forge/plugin-webpack";
+import type { ForgeConfig } from '@electron-forge/shared-types';
+import { MakerSquirrel } from '@electron-forge/maker-squirrel';
+import { MakerZIP } from '@electron-forge/maker-zip';
+import { MakerDeb } from '@electron-forge/maker-deb';
+import { MakerRpm } from '@electron-forge/maker-rpm';
+import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
-import { mainConfig } from "./webpack.main.config";
-import { rendererConfig } from "./webpack.renderer.config";
+import { mainConfig } from './webpack.main.config';
+import { rendererConfig } from './webpack.renderer.config';
 
 const config: ForgeConfig = {
   packagerConfig: {
-    extraResource: ["./src/assets/app.json"],
+    extraResource: ['./src/assets/app.json'],
+    icon: './src/assets/damage-software-256',
   },
   rebuildConfig: {},
   makers: [
-    new MakerSquirrel({setupIcon: "./src/assets/damage-software-256.ico"}),
-    new MakerZIP({}, ["darwin"]),
+    new MakerSquirrel({
+      authors: 'Damage Software',
+      description: 'Damage Software ',
+      iconUrl:
+        'https://raw.githubusercontent.com/nzhul/launcher/master/src/assets/damage-software-256.ico',
+      setupIcon: './src/assets/damage-software-256.ico',
+    }),
+    new MakerZIP({}, ['darwin']),
     new MakerRpm({}),
     new MakerDeb({}),
   ],
@@ -28,19 +35,19 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: "./src/index.html",
-            js: "./src/renderer.ts",
-            name: "main_window",
+            html: './src/index.html',
+            js: './src/renderer.ts',
+            name: 'main_window',
             preload: {
-              js: "./src/preload.ts",
+              js: './src/preload.ts',
             },
           },
           {
-            html: "./src/splash/splash.html",
-            js: "./src/splash/renderer.ts",
-            name: "splash",
+            html: './src/splash/splash.html',
+            js: './src/splash/renderer.ts',
+            name: 'splash',
             preload: {
-              js: "./src/splash/preload.ts",
+              js: './src/splash/preload.ts',
             },
           },
         ],
